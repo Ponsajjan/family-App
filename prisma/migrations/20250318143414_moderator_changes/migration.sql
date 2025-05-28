8432ea605b86973ca60bc130020347cc1497a366
Ponsajjan <saju.sajjan@gmail.com>
2025-05-28 22:13:05 +0530

    schama updated

--- prisma/migrations/20250318143414_moderator_changes/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "RequestDetails" ADD COLUMN "descendantOf" TEXT;
-ALTER TABLE "RequestDetails" ADD COLUMN "gender" TEXT;
-ALTER TABLE "RequestDetails" ADD COLUMN "name" TEXT;
--- prisma/migrations/20250528163855_initial_migration/migration.sql
+++ prisma/migrations/20250528163855_initial_migration/migration.sql
@@ -2,8 +2,11 @@
 CREATE TABLE "Auth" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "forDescendanceOf" TEXT NOT NULL,
-    "memberId" INTEGER NOT NULL,
-    "password" TEXT NOT NULL
+    "mainMemberId" INTEGER,
+    "moderatorPassword" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
@@ -19,12 +22,13 @@ CREATE TABLE "Member" (
     "deathDate" INTEGER,
     "deathMonth" INTEGER,
     "deathYear" INTEGER,
-    "gender" TEXT,
+    "gender" TEXT NOT NULL,
     "phoneNumber" TEXT,
     "address" TEXT,
     "occupation" TEXT,
     "education" TEXT,
     "descendant" BOOLEAN NOT NULL DEFAULT false,
+    "order" INTEGER NOT NULL DEFAULT 1,
     "fatherId" INTEGER,
     "motherId" INTEGER,
     "partnerId" INTEGER,
@@ -32,15 +36,20 @@ CREATE TABLE "Member" (
     "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "Member_fatherId_fkey" FOREIGN KEY ("fatherId") REFERENCES "Member" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT "Member_motherId_fkey" FOREIGN KEY ("motherId") REFERENCES "Member" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
-    CONSTRAINT "Member_partnerId_fkey" FOREIGN KEY ("partnerId") REFERENCES "Member" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    CONSTRAINT "Member_partnerId_fkey" FOREIGN KEY ("partnerId") REFERENCES "Member" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Member_descendantOf_fkey" FOREIGN KEY ("descendantOf") REFERENCES "Auth" ("forDescendanceOf") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "MemberEditRequest" (
+CREATE TABLE "RequestDetails" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "descendantOf" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
     "details" TEXT NOT NULL,
     "memberId" INTEGER NOT NULL,
-    CONSTRAINT "MemberEditRequest_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "RequestDetails_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -55,8 +64,25 @@ CREATE TABLE "nonDescendantRelation" (
     CONSTRAINT "nonDescendantRelation_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Member" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "ModeratorList" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "moderatorName" TEXT NOT NULL,
+    "moderatorContact" TEXT NOT NULL,
+    "authId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "ModeratorList_authId_fkey" FOREIGN KEY ("authId") REFERENCES "Auth" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
-CREATE UNIQUE INDEX "Auth_memberId_key" ON "Auth"("memberId");
+CREATE UNIQUE INDEX "Auth_forDescendanceOf_key" ON "Auth"("forDescendanceOf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Auth_mainMemberId_key" ON "Auth"("mainMemberId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Auth_password_key" ON "Auth"("password");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "nonDescendantRelation_memberId_key" ON "nonDescendantRelation"("memberId");
