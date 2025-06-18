2e942432739408481588e44551c4193f6da3412a
Ponsajjan <saju.sajjan@gmail.com>
2025-06-18 10:06:13 +0530

    fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-      <div className="text-center text-text_color pt-24 w-full loading-text">
+      <div className="text-center text-text_color pt-10 md:pt-24 w-full loading-text">
         Loading...
       </div>
     )
