e4850025ad98f71aa0e01c5a58dc3b8c63b6fd09
Ponsajjan <saju.sajjan@gmail.com>
2025-06-21 08:52:51 +0530

    form button disable

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -6,27 +6,35 @@ import { useAuth } from "@/contexts/AuthContext";
 
 export default function LoginForm() {
     const [form, setForm] = useState({ password: "" });
-    const [error, setError] = useState("");
+    const [message, setMessage] = useState("");
+    const [submitting, setSubmitting] = useState(false);
     const {token, storeLoginValues} = useAuth();
 
     const handelInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setForm({ ...form, [e.target.name]: e.target.value });
-        setError("");
+        setMessage("");
     };
 
     async function handleSubmit(formData: FormData) {
+        if (!formData.get("password")) {
+            return;
+        }
         try {
+            setSubmitting(true);
+            setMessage("Submitting...");
             const result = await login(formData);
             if (result.success && result.token) {
                 storeLoginValues(result.token, result.userType);
             } else {
-                setError(result.error || "Login failed");
+                setMessage(result.error || "Login failed");
                 if (result.error === "Invalid credential") {
                     setForm({ ...form, password: "" });
                 }
             }
         } catch (error: any) {
-            setError(error.message);
+            setMessage(error.message);
+        } finally {
+            setSubmitting(false);
         }
     }
     if (!token) {
@@ -101,38 +109,36 @@ export default function LoginForm() {
                     c8.537-1.857,15.894-5.967,20.343-13.665c2.527-4.376,4.318-9.354,5.181-14.34C421.442,286.777,408.88,288.662,398.489,293.362z"/>
             </svg>
             </div>
-            <div className="w-full max-w-80 relative pb-6">
+            <div className="w-full max-w-80">
                 <form action={handleSubmit}>
-                    <div className={`flex h-12 border border-border_color ${error ? 'passwordError' : ''} bg-field_color rounded-md overflow-hidden px-2`}>
+                    <div className={`flex h-12 border border-border_color ${message ? 'passwordError' : ''} bg-field_color rounded-md overflow-hidden px-2`}>
                         <label className="flex items-center w-full">
                         <input
                             name="password"
                             onChange={(e) => handelInputChange(e)}
                             value={form.password || ""}
-                            
+                            disabled={submitting}
                             required
                             autoFocus
                             placeholder="hello world !"
                             className="py-3 px-1 outline-none text-text_color focus:border-border_active text-sm h-full w-full bg-transparent disabled:cursor-not-allowed"
                         />
                         </label>
-                        <button type="submit">
-                        <svg
-                            width="40px"
-                            height="40px"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 48 48"
-                        >
-                            <path d="M38.8,26.1c-5.9,4.2-16.2,2.6-16.2,5.7s.6,11.1.6,11.1L18.8,44s.6-9.5.6-12.2-7-1.1-10.8-3.1S3.7,16.6,4.2,14c3.3,2.6,5.4,2.6,9.8,4.2s6,5.6,6.7,8.2c0,.1-7.7-1.4-12.1-5.5C10,25,15,26.6,17.2,27.1A4.7,4.7,0,0,1,21,29.7c.5-2.1,5.3-6.8,9.1-8.4s7.6-4.2,8.2-7.1c-4.3,4-8.1,4.1-11.9,6.6a16.3,16.3,0,0,0-4.6,4.5c.6-2.8,2.3-9.6,5.7-12.9C34.1,6.1,38.3,8.7,43.7,4,44.2,5.6,44.8,21.9,38.8,26.1Z" />
-                        </svg>
+                        <button type="submit" disabled={submitting} className={`py-1 ${submitting ? 'cursor-not-allowed opacity-60' : ''}`}>
+                            <svg
+                                width="40px"
+                                height="40px"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 48 48"
+                            >
+                                <path d="M38.8,26.1c-5.9,4.2-16.2,2.6-16.2,5.7s.6,11.1.6,11.1L18.8,44s.6-9.5.6-12.2-7-1.1-10.8-3.1S3.7,16.6,4.2,14c3.3,2.6,5.4,2.6,9.8,4.2s6,5.6,6.7,8.2c0,.1-7.7-1.4-12.1-5.5C10,25,15,26.6,17.2,27.1A4.7,4.7,0,0,1,21,29.7c.5-2.1,5.3-6.8,9.1-8.4s7.6-4.2,8.2-7.1c-4.3,4-8.1,4.1-11.9,6.6a16.3,16.3,0,0,0-4.6,4.5c.6-2.8,2.3-9.6,5.7-12.9C34.1,6.1,38.3,8.7,43.7,4,44.2,5.6,44.8,21.9,38.8,26.1Z" />
+                            </svg>
                         </button>
                     </div>
                 </form>
-                {error && (
-                <p className="text-text_color text-sm absolute bottom-0 left-2">
-                    {error}
-                </p>
-                )}
+                <div className="text-text_color text-sm min-h-6">
+                    {message}
+                </div>
             </div>
             </div>
         );
