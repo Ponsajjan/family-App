7fdc134cd945e589f62fd2dd044f99e03cc5836c
Ponsajjan <saju.sajjan@gmail.com>
2025-07-10 07:39:28 +0530

    added offline animation

--- src/app/~offline/page.tsx
+++ src/app/~offline/page.tsx
@@ -2,15 +2,15 @@ import { Offline } from "@/utils/Icons";
 
 export default function Index() {
   return (
-    <div className="w-full flex flex-col justify-center min-h-[calc(100vh-3rem)] items-center bg-main_background">
+    <div className="w-full flex flex-col justify-center min-h-[calc(100vh-6rem)] items-center bg-main_background">
       <div className="max-w-4xl mx-auto">
         <div className="z z-1 text-text_color text-4xl">Z</div>
         <div className="z z-2 text-text_color text-2xl">z</div>
         <div className="z z-3 text-text_color text-xl">z</div>
         <div className="z z-4 text-text_color text-base">z</div>
       </div>
-      <Offline />
-      <p className="text-center text-lg text-text_color mt-3">-- Offline --</p>
+      <span className="sleep-animation"><Offline /></span>
+      <p className="text-center text-lg text-text_color mt-3 font-semibold">-- 0ffline --</p>
     </div>
   );
 }
