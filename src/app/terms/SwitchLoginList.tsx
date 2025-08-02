f536462b2158c3118559c206f9c8239aba36e42b
Ponsajjan <saju.sajjan@gmail.com>
2025-08-02 19:24:57 +0530

    toggle switch improvement

--- src/app/terms/SwitchLoginList.tsx
+++ src/app/terms/SwitchLoginList.tsx
@@ -1,4 +1,3 @@
-import { HoldTextButton } from '@/components/HoldButton';
 import ToggleSwitch from '@/components/ToggleSwitch';
 import React, { useState } from 'react'
 
@@ -21,14 +20,14 @@ function SwitchLoginList() {
         <div className='p-4'>
           {family.map((m_member, index) => {
             return (
-              <HoldTextButton onClick={() => handleToggleChange(m_member)} className={`w-full ${activeFamilyMember === m_member ? '' : 'py-0.5'}`} key={index}>
-                <div className={`w-full flex items-center justify-between transform transition-all duration-200 ${activeFamilyMember === m_member ? 'font-medium text-base scale-[1.02] shadow-md min-h-[41px] ' : 'min-h-[40px] opacity-70'} bg-field_color text-text_color border border-l-4 border-border_color rounded-md cursor-pointer`}>
+              <div onClick={() => handleToggleChange(m_member)} className={`${activeFamilyMember === m_member ? '' : 'py-0.5'}`} key={index}>
+                <div className={`flex items-center justify-between transform transition-all duration-200 ${activeFamilyMember === m_member ? 'font-medium text-base scale-[1.02] shadow-md min-h-[41px] ' : 'min-h-[40px] opacity-70'} bg-field_color text-text_color border border-l-4 border-border_color rounded-md cursor-pointer`}>
                   <div className='px-3'>{m_member}</div>
                   <ToggleSwitch 
                     isActive={activeFamilyMember === m_member}
                   />
                 </div>
-              </HoldTextButton>
+              </div>
             );
           })}
           <div className='flex justify-between items-center cursor-pointer mt-6 px-4 py-1 border border-border_color rounded-lg text-text_color'>
