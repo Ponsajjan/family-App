c72f484aababc5708c328e5873967f5e1e478402
Ponsajjan <saju.sajjan@gmail.com>
2025-12-02 09:52:08 +0530

    side nav fix

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -33,7 +33,7 @@ export default function ClientLayout({
       <main id="MainDiv">
         <noscript>Amor fati!..</noscript>
         <AuthProvider>
-          <div className="w-full bg-field_color/95 transition-all duration-500 ease-in-out">
+          <div className="w-full transition-all duration-500 ease-in-out">
             <div className="w-full max-w-[2600px] mx-auto md:border-x md:border-border_color md:min-h-screen relative flex">
               <Sidenav />
               <div className="w-full relative">
