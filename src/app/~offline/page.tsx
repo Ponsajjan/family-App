42bbafa7a363dab9c43a5d550a623e9ff3828283
Ponsajjan <saju.sajjan@gmail.com>
2025-12-02 12:03:40 +0530

    offline ui fix

--- src/app/~offline/page.tsx
+++ src/app/~offline/page.tsx
@@ -4,7 +4,7 @@ import { Offline } from "@/utils/Icons";
 export default function Index() {
   return (
     <Container className="flex flex-col justify-center items-center h-full">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-4xl mx-auto mt-24">
         <div className="z z-1 text-text_color text-4xl">Z</div>
         <div className="z z-2 text-text_color text-2xl">z</div>
         <div className="z z-3 text-text_color text-xl">z</div>
