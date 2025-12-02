b694812c10d876366882f3b06f1e4d32e027deda
Ponsajjan <saju.sajjan@gmail.com>
2025-12-02 18:41:43 +0530

    error scroll

--- src/components/forms/EditMemberForm.tsx
+++ src/components/forms/EditMemberForm.tsx
@@ -20,6 +20,7 @@ function EditMemberForm({
 
     const toast = useToast();
     const showWarning = (input: string) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
         toast?.show(`Can not change ${input} for this member`, "warning", 5000);
     }
 
