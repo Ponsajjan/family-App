f9fd4a976c2124f49b7a4318981621a55c8c7386
Ponsajjan <saju.sajjan@gmail.com>
2025-12-06 23:17:33 +0530

    infinit scroll fix

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,13 @@
-function Container({ className = '', children }: any) {
+import { forwardRef } from 'react';
+
+const Container = forwardRef<HTMLDivElement, any>(({ className = '', children }, ref) => {
   return (
-    <div className={`md:h-[calc(100vh-3rem)] md:overflow-y-auto w-full ${className}`}>
+    <div ref={ref} className={`md:h-[calc(100vh-3rem)] md:overflow-y-auto w-full ${className}`}>
       {children}
     </div>
   );
-}
+});
+
+Container.displayName = 'Container';
 
 export default Container;
