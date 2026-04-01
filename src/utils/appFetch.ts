ba7c081a21a68101db3acc9eaf71c4330792d3ac
Ponsajjan <saju.sajjan@gmail.com>
2026-04-01 19:18:13 +0530

    updated error message

--- src/utils/appFetch.ts
+++ src/utils/appFetch.ts
@@ -1,6 +1,6 @@
 export const appFetch = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
   if (typeof navigator !== 'undefined' && !navigator.onLine) {
-    throw new Error("You are offline");
+    throw new Error("No internet connection. Retry when you are back online");
   }
 
   const response = await fetch(input, init);
@@ -8,7 +8,7 @@ export const appFetch = async (input: RequestInfo | URL, init?: RequestInit): Pr
   const contentType = response.headers.get('content-type');
   if (contentType && contentType.includes('text/html')) {
     // If the service worker intercepted our API request and responded with the offline fallback HTML
-    throw new Error("You are offline");
+    throw new Error("No internet connection. Retry when you are back online");
   }
 
   return response;
