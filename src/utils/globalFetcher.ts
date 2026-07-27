79d56256e9f7965b4f9c4e43dbe4e75129683bcb
Ponsajjan <sajjan@mavencart.com>
2026-07-27 08:55:15 +0530

    offline error msg fetch fix

--- src/utils/globalFetcher.ts
+++ src/utils/globalFetcher.ts
@@ -1,7 +1,8 @@
-import { appFetch } from './appFetch';
+import { OFFLINE_MESSAGE } from "./appFetch";
 
 export const globalFetcher = async (url: string) => {
-  const res = await appFetch(url, {
+
+  const res = await fetch(url, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
@@ -11,8 +12,16 @@ export const globalFetcher = async (url: string) => {
   }
 
   if (!res.ok) {
+    const contentType = res.headers.get('content-type');
     const errorData = await res.json().catch(() => ({}));
-    throw new Error(errorData.error || "Failed to fetch data");
+    if (typeof navigator !== 'undefined' && !navigator.onLine) {
+      throw new Error(OFFLINE_MESSAGE);
+    } else if (contentType && contentType.includes('text/html')) {
+      // Check that the Service Worker didn't intercept and return the offline HTML page
+      throw new Error(OFFLINE_MESSAGE);
+    } else {
+      throw new Error(errorData.error || "Failed to fetch data");
+    }
   }
 
   const json = await res.json();
